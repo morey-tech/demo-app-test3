c505334b3f22c75b6551ff6928779fa001fb4fee
Nicholas Morey <nicholas@morey.tech>
2023-02-28 15:26:05 -0500

    feat: print secret and name

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ import (
 
 func main() {
 	myFigure := figure.NewColorFigure("morey-tech is Awesome!!!!!", "larry3d", "yellow", true)
+	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
-		myFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "yellow", true)
+		mySecretFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "yellow", true)
+		mySecretFigure.Print()
 	}
-	myFigure.Print()
 	time.Sleep(10 * time.Hour)
 }
