9ce8b4c8577c48ccf33c5efd72ab90a3caaa4030
Nicholas Morey <nicholas@morey.tech>
2023-02-28 15:26:22 -0500

    feat: use red for secret

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
-		mySecretFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "yellow", true)
+		mySecretFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "red", true)
 		mySecretFigure.Print()
 	}
 	time.Sleep(10 * time.Hour)
